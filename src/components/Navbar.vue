a7b62c40d4d585880883dc57497d530bc5c394c2
Simon Vriesema <simon.vriesema@icloud.com>
2022-09-22 18:47:24 +0200

    did some tweaks

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,7 @@
         <p>Don't be vague, ask for the Auctioneer.</p>
       </div>
       <div class="w-1/3 align-middle items-end`">
-        <a href="#" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">Sign in</a>
+        <a href="#" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">Sign up</a>
         <a href="#"
           class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Login</a>
       </div>
--- src/components/offers/Overview32.vue
+++ src/components/offers/Overview32.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="container mx-auto flex-col">
-    <div>
+  <!--  <div class="">-->
+  <!--    <div class="">-->
+  <!--      <div class="container mx-auto">-->
+  <!--        <h4 class="float-left font-medium leading-tight text-2xl mt-0 mb-2">List of all offers:</h4>-->
+  <!--      </div>-->
+  <!--      <div class="columns-2 h-full gap-2">-->
+  <!--        <div class="w-full">-->
+  <!--          <table class="text-sm text-left text-gray-500 dark:text-gray-400">-->
+  <!--            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">-->
+  <!--            <tr>-->
+  <!--              <th scope="col" class="py-3 px-6">-->
+  <!--                <div class="flex items-center">-->
+  <!--                  Title-->
+  <!--                  <a href="#">-->
+  <!--                    <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor"-->
+  <!--                         viewBox="0 0 320 512">-->
+  <!--                      <path-->
+  <!--                          d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z">-->
+  <!--                      </path>-->
+  <!--                    </svg>-->
+  <!--                  </a>-->
+  <!--                </div>-->
+  <!--              </th>-->
+  <!--            </tr>-->
+  <!--            </thead>-->
+  <!--            <tbody>-->
+  <!--            <tr v-for="(item) in offers" :key="item.id">-->
+  <!--               {{ item.message }}-->
+  <!--              <td v-on:click="selectItem(item)" class="py-4 px-6 hover:bg-gray-100 hover:text-black" :class="{'bg-blue-700 text-white':selectedOrder?.id===item.id}">-->
+  <!--                {{ item.title }}-->
+  <!--              </td>-->
+  <!--            </tr>-->
+  <!--            </tbody>-->
+  <!--          </table>-->
+  <!--        </div>-->
+  <!--        <div class="w-full">-->
+  <!--          <Detail32 v-if="selectedOrder" v-bind:item="selectedOrder"/>-->
+  <!--          <div v-else>-->
+  <!--            <p>Select an offer at the left</p>-->
+  <!--          </div>-->
+  <!--        </div>-->
+  <!--      </div>-->
 
-<!--      <div class="container mx-auto">-->
-<!--        <h4 class="float-left font-medium leading-tight text-2xl mt-0 mb-2">Overview of all offered articles</h4>-->
-<!--      </div>-->
-      <div class="columns-2 h-full gap-2">
-        <div class="w-full">
-          <table class="text-sm text-left text-gray-500 dark:text-gray-400">
+  <!--    </div>-->
+  <!--  </div>-->
+  <div class="max-w-screen-xl flex flex-row m-auto">
+
+    <div class="w-1/4 flex flex-col">
+
+      <div class="container mx-auto">
+        <h4 class="float-left font-medium leading-tight text-xl mt-0 mb-2">Overview of all offered articles:</h4>
+      </div>
+
+      <div class="container mx-auto">
+        <div class="relative shadow-md sm:rounded-lg">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" class="py-3 px-6">
                 <div class="flex items-center">
-                  Title
+                  Offer Title:
                   <a href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor"
                          viewBox="0 0 320 512">
@@ -26,30 +73,39 @@
             </tr>
             </thead>
             <tbody>
+
             <tr v-for="(item) in offers" :key="item.id">
-               {{ item.message }}
-              <td v-on:click="selectItem(item)" class="py-4 px-6 hover:bg-gray-100 hover:text-black" :class="{'bg-blue-700 text-white':selectedOrder?.id===item.id}">
+              {{ item.message }}
+              <td v-on:click="selectItem(item)" class="py-4 px-6 hover:bg-blue-100 hover:text-black"
+                  :class="{'bg-blue-500 text-white':selectedOrder?.id===item.id}">
                 {{ item.title }}
               </td>
             </tr>
             </tbody>
           </table>
         </div>
-        <div class="w-full">
-          <Detail32 v-if="selectedOrder" v-bind:item="selectedOrder"/>
-          <div v-else>
-            <p>Select an offer at the left</p>
-          </div>
+        <div id="newOfferBtn">
+          <button v-on:click="onNewOffer" type="button"
+                  class="float-right text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-3">
+            <i class="fa fa-plus"></i> New Offer
+          </button>
         </div>
       </div>
 
     </div>
+
+    <div id="detail">
+
+      <Detail32 v-if="selectedOrder" v-bind:item="selectedOrder"/>
+
+    </div>
+
   </div>
 
 </template>
 
 <script>
-import { Offer } from "@/models/offer";
+import {Offer} from "@/models/offer";
 import Detail32 from "./Detail32.vue";
 
 export default {
