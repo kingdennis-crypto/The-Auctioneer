e57318ecffb5ae06c531ce587c5fd1fa3fcd81e3
Simon Vriesema <simon.vriesema@icloud.com>
2022-09-26 18:37:28 +0200

    doing 3.3.2 B

---
+++ src/components/offers/Overview33.vue
@@ -0,0 +1,103 @@
+/* vue/no-mutating-props */
+<template>
+  <div class="container mx-auto">
+    <div class="flex flex-row gap-5">
+      <div class="w-1/4">
+        <!-- TODO add a scroll to the table of it exceeds max height -->
+        <table class="w-full mb-3 text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th class="py-5 px-6">Offer title</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item) in offers" :key="item.id">
+            <td @click="selectItem(item)"
+                class="py-4 px-6 font-medium whitespace-nowrap dark:text-white cursor-pointer rounded-sm [&:not(.bg-blue-500)]:hover:bg-gray-100"
+                :class="[ selectedOffer?.id === item.id ? selectedRowStyle : notSelectedRowStyle ]">
+              {{ item.title }}
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <button class="bg-blue-500 w-full p-3 rounded-md text-white cursor-pointer font-medium hover:bg-blue-600"
+                @click="onNewOffer">
+          Add offer
+        </button>
+      </div>
+      <div class="w-3/4">
+        <div class="relative h-full">
+          <router-view v-if="selectedOffer" v-bind:item="selectedOffer" @delete-selected="deleteOffer()" />
+          <div v-else class="bg-violet-500 h-full w-full absolute flex rounded-md">
+            <p class="text-white text-5xl font-bold m-auto">No offer was selected</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Offer } from "@/models/offer";
+
+export default {
+  name: "OffersOverview33",
+  components: {},
+
+  created() {
+    for (let i = 0; i < 8; i++) {
+      this.offers.push(Offer.createSampleOffer(this.nextId));
+      this.nextId = this.nextId + 3;
+    }
+  },
+
+  data() {
+    return {
+      nextId: 30000,
+      offers: [],
+      selectedOffer: null,
+
+      selectedRowStyle: "bg-blue-500 text-white",
+      notSelectedRowStyle: "text-gray-900"
+    }
+  },
+
+  methods: {
+    onNewOffer: function () {
+      const newOffer = Offer.createSampleOffer(this.nextId);
+      this.selectedOffer = newOffer
+
+      this.offers.push(newOffer);
+      this.nextId = this.nextId + 3;
+    },
+
+    selectItem(element) {
+      if (element === this.selectedOffer) {
+        return null;
+      }
+
+      this.selectedOffer = element;
+    },
+
+    deleteOffer() {
+      if (confirm("Are you sure you want to delete this offer?")) {
+        this.offers = this.offers.filter(element => element.id !== this.selectedOffer.id)
+        this.selectedOffer = null;
+      }
+    },
+
+    onSelect(offer) {
+      if (offer != null && offer !== this.selectedOffer) {
+        this.$router.push(this.$route.matched[0].path + "/" + offer.id)
+      } else if (this.selectedOffer != null) {
+        // TO DO
+        this.$router.push(this.$route.matched[0].path + "/" + offer.id)
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,20 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 // Components
 import WelcomeVue from '@/components/Welcome.vue';
+import Overview33Vue from '@/components/offers/Overview33.vue';
 import Overview32Vue from '@/components/offers/Overview32.vue';
 import Overview31Vue from '@/components/offers/Overview31.vue';
 import UnkownRoute from '@/components/UnknownRoute.vue';
+import Detail32 from "@/components/offers/Detail32.vue";
 
 const routes = [
   { path: '/', component: WelcomeVue },
   { path: '/offers/overview31', component: Overview31Vue },
   { path: '/offers/overview32', component: Overview32Vue },
-  { path: '/:pathMatch(.*)', component: UnkownRoute }
+  { path: '/offers/overview33', component: Overview33Vue, children: [
+      { path: ":id", component: Detail32}
+    ]},
+  { path: '/:pathMatch(.*)', component: UnkownRoute },
 ];
 
 export const router = createRouter({
