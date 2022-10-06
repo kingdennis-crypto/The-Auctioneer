4765a2d593f84e749e5995168c3a968a3754a081
Simon Vriesema <simon.vriesema@icloud.com>
2022-10-06 16:02:11 +0200

    vegetarische loempia 2

---
+++ public/auc-server/src/test/java/wf/aucserver/AucServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.aucserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AucServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
