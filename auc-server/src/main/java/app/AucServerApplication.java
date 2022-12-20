9a1da6880cda2b939563e0d207f55ad0286055ce
Simon Vriesema <simon.vriesema@icloud.com>
2022-12-20 16:00:32 +0100

    finished 4.1.1

--- auc-server/src/main/java/app/AucServerApplication.java
+++ auc-server/src/main/java/app/AucServerApplication.java
@@ -1,15 +1,22 @@
 package app;
 
+import app.models.Offer;
+import app.repositories.OffersRepositoryJpa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootApplication
-public class AucServerApplication {
+public class AucServerApplication implements CommandLineRunner {
 
-//	@Autowired
-//	private OffersRepositoryJpa offersRepo;
-//
+	@Autowired
+	private OffersRepositoryJpa offersRepo;
+
 //	@Autowired
 //	private BidsRepositoryJpa bidsRepo;
 
@@ -18,25 +25,25 @@ public class AucServerApplication {
 //		OffersRepositoryMockImpl offersRepositoryMock = applicationContext.getBean(OffersRepositoryMockImpl.class);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//		System.out.println("Running CommandLine Startup");
-//
-//		this.createInitialOffers();
-//		this.createInitialBids();
-//	}
-//
-//	public void createInitialOffers() {
-//		List<Offer> offers = this.offersRepo.findAll();
-//
-//		if (offers.size() > 0) return;
-//
-//		System.out.println("Configuring some initial Offer data");;
-//
-//		for (int i = 0; i < 9; i++) {
-//			Offer offer = offersRepo.save(Offer.createSampleOffer(0));
-//		}
-//	}
+	@Transactional
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Running CommandLine Startup");
+
+		this.createInitialOffers();
+	}
+
+	public void createInitialOffers() {
+		List<Offer> offers = this.offersRepo.findAll();
+
+		if (offers.size() > 0) return;
+
+		System.out.println("Configuring some initial Offer data");;
+
+		for (int i = 0; i < 9; i++) {
+			Offer offer = offersRepo.save(Offer.createSampleOffer(0));
+		}
+	}
 //
 //	public void createInitialBids() {
 //		List<Bid> bids = this.bidsRepo.findAll();
--- auc-server/src/main/java/app/models/Offer.java
+++ auc-server/src/main/java/app/models/Offer.java
@@ -1,32 +1,51 @@
 package app.models;
 
+//import app.repositories.Identifiable;
 import app.views.CustomOfferView;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Random;
+import java.util.*;
 
+@Entity
 public class Offer {
-    private static final String[] TITLES = {"toolset", "lamp", "cabinet", "clock", "bicycle", "coat"};
-    private static final String[] STATUSSES = {"NEW", "FOR_SALE", "SOLD", "PAID", "DELVIERED", "CLOSED", "EXPIRED", "WITHDRAWN"};
+    private static final String[] TITLES = {"toolset", "lamp", "lamp", "cabinet", "lamp", "clock", "bicycle", "coat"};
     private static final String[] DESCRIPTIONS = {"A characteristic, original toolset", "A modern comfort lamp", "A small comfortable lamp", "An antique cozy cabinet", "An antique cozy lamp", "A characteristic, cozy clock", "A small, robust bicycle", "A characteristic, original coat"};
 
-    @JsonView(CustomOfferView.Summary.class)
-    private int id;
+    public enum STATUS {
+        NEW, FOR_SALE, SOLD, PAID, DELIVERED, CLOSED, EXPIRED, WITHDRAWN
+    };
 
+    @JsonView(CustomOfferView.Summary.class)
+    @Id
+    private long id;
     @JsonView(CustomOfferView.Summary.class)
     private String title;
-
     @JsonView(CustomOfferView.Summary.class)
-    private String status;
-
+    @Enumerated(EnumType.ORDINAL)
+    private STATUS status;
+    @JsonView(CustomOfferView.Summary.class)
     private String description;
+    @JsonView(CustomOfferView.Summary.class)
     private LocalDate sellDate;
+    @JsonView(CustomOfferView.Summary.class)
     private int valueHighestBid;
 
-    public Offer(int id, String title, String status, String description, LocalDate sellDate, int valueHighestBid) {
+    public Offer(int id) {
         this.id = id;
 
+        Offer newOffer = this.createSampleOffer(id);
+        this.title = newOffer.title;
+        this.status = newOffer.status;
+        this.description = newOffer.description;
+        this.sellDate = newOffer.sellDate;
+        this.valueHighestBid = newOffer.valueHighestBid;
+    }
+
+    public Offer(int id, String title, STATUS status, String description, LocalDate sellDate, int valueHighestBid) {
+        this.id = id;
         this.title = title;
         this.status = status;
         this.description = description;
@@ -34,29 +53,46 @@ public class Offer {
         this.valueHighestBid = valueHighestBid;
     }
 
-    public static Offer createSampleOffer(int id) {
-        Random random = new Random();
-
-        LocalDate sellDate = LocalDate.now();
-        int valueHighestBid = random.nextInt(1000);
-
-        String title = TITLES[random.nextInt(TITLES.length)];
-        String status = STATUSSES[random.nextInt(STATUSSES.length)];
-        String description = DESCRIPTIONS[random.nextInt(DESCRIPTIONS.length)];
-
-        return new Offer(id, title, status, description, sellDate, valueHighestBid);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    public Offer() { super(); }
 
     public Offer copyConstructor(Offer offer) {
         return offer;
+    }
+
+    public static Offer createSampleOffer(int pId) {
+        if (pId == 0) pId = new Random().nextInt(40000, 50000);
+
+        LocalDate sellDate = LocalDate.now();
+        int valueHighestBid = (int) Math.floor(Math.random() * 100);
+
+        String title = TITLES[(int) Math.floor(Math.random() * TITLES.length)];
+        STATUS status = STATUS.NEW;
+        String description = DESCRIPTIONS[(int) Math.floor(Math.random() * DESCRIPTIONS.length)];
+
+        return new Offer(pId, title, status, description, sellDate, valueHighestBid);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offer offer = (Offer) o;
+        return id == offer.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+//    @Override
+    public long getId() {
+        return id;
+    }
+
+//    @Override
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -67,11 +103,11 @@ public class Offer {
         this.title = title;
     }
 
-    public String getStatus() {
+    public STATUS getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(STATUS status) {
         this.status = status;
     }
 
@@ -99,3 +135,5 @@ public class Offer {
         this.valueHighestBid = valueHighestBid;
     }
 }
+
+
